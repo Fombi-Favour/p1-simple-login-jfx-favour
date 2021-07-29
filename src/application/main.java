c8dee8e740dd668132a12bfdbd28deb9ad48ee9a
Fombi Favour <fombifavour@gmail.com>
2021-07-29 04:14:31 +0100

    default commit by adding JDBC

--- src/application/main.java
+++ src/application/main.java
@@ -1,6 +1,7 @@
 package application;
 
 
+import JDBC.sqlJava;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -8,10 +9,15 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class main extends Application {
 
@@ -34,6 +40,24 @@ public class main extends Application {
         VBox vbox = new VBox(label, input);
         vbox.setSpacing(5);
         vbox.setAlignment(Pos.CENTER);
+
+        Label lbl = new Label("First Name");
+        TextField plot = new TextField(); input.setPrefWidth(stage.getWidth() / 4);
+        plot.setPromptText("first name");
+        VBox cop = new VBox(lbl, plot);
+        cop.setSpacing(5);
+        cop.setAlignment(Pos.CENTER);
+
+        Label lbl1 = new Label("Last Name");
+        TextField plot1 = new TextField(); input.setPrefWidth(stage.getWidth() / 4);
+        plot1.setPromptText("last name");
+        VBox cop1 = new VBox(lbl1, plot1);
+        cop1.setSpacing(5);
+        cop1.setAlignment(Pos.CENTER);
+
+        HBox copPane = new HBox(cop, cop1);
+        copPane.setSpacing(10);
+        copPane.setAlignment(Pos.CENTER);
 
         Label label0 = new Label("Email address");
         TextField input0 = new TextField();
@@ -58,29 +82,38 @@ public class main extends Application {
         vbox2.setSpacing(5);
         vbox2.setAlignment(Pos.CENTER);
 
-        TextField[] allInput = {input, input0, pw, pw1};
+        TextField[] allInput = {input, plot, plot1, input0, pw, pw1};
 
         Button button = new Button("Create Account");
         Font font1 = Font.font("Helvetica", FontWeight.EXTRA_BOLD, 16);
         button.setFont(font1);
         button.setStyle("-fx-color: Green");
         button.setOnAction(actionEvent -> {
-            String errorMessage = "";
-            for(TextField eachInput : allInput){
-                if(eachInput.getText().length() == 0){
-                    eachInput.setPromptText("Fill out this field");
-                    errorMessage = errorMessage + "";
-                }
-                else{
-                    stage.close();
-                }
+
+            sqlJava store = new sqlJava();
+            Connection conn = sqlJava.getConnection();
+
+            PreparedStatement pst = null;
+            String sql = "INSERT INTO accountemy VALUES(?, ?, ?, ?, ?, ?)";
+            try {
+                pst = conn.prepareStatement(sql);
+                pst.setString(1, input.getText());
+                pst.setString(2, plot.getText());
+                pst.setString(3, plot1.getText());
+                pst.setString(4, input0.getText());
+                pst.setString(5, pw.getText());
+                pst.setString(6, pw1.getText());
+
+                pst.executeUpdate();
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
         });
 
 
 
         VBox mainPane = new VBox();
-        mainPane.getChildren().addAll(intro, intro0, vbox, vbox0, vbox1, vbox2, button);
+        mainPane.getChildren().addAll(intro, intro0, copPane, vbox, vbox0, vbox1, vbox2, button);
         mainPane.setSpacing(15);
         mainPane.setAlignment(Pos.CENTER);
 
